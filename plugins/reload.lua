151417d3ee87d083128561da2782f49963fa00d4
climaxit <climaxit3@gmail.com>
2016-12-14 09:48:19 -0800

    Update reload.lua

--- plugins/reload.lua
+++ plugins/reload.lua
@@ -2,7 +2,7 @@ do
   local function reload_plugins(msg)
     plugins = {}
     load_plugins()
-    local text = "_#BOT_ *Reloaded!*\n*#All* _PLugins_ *Reloaded!*\n_#All_ *Changes* _Succesfully_ *Installed.*\n*#TeleBeyond* _Is Ready._"
+    local text = "_BOT_ *Reloaded!*\n*#All* _PLugins_ *Reloaded!*\n_All_ *Changes* _Succesfully_ *Installed.*\n*Climax IT* _Is Ready._"
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
   end
 function run(msg, matches)
